49715836cefde9c0123b5f7f9af61ccc2e2c0013
ImlBeta <millan520@hotmail.com>
2021-11-16 14:00:57 -0600

    Login Botones_Icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -84,9 +84,20 @@
           <span class="mock-block">Block</span>
         </a-space>
       </div>
+      <p></p>
+      <br />
+      <a-breadcrumb>
+        <a-breadcrumb-item href="">
+          <a-icon type="home" />
+        </a-breadcrumb-item>
+        <a-breadcrumb-item href="">
+          <a-icon type="user" />
+          <span>Application List</span>
+        </a-breadcrumb-item>
+        <a-breadcrumb-item> Application </a-breadcrumb-item>
+      </a-breadcrumb>
     </div>
   </div>
-
 </template>
 
 <script>
